652f13630878c805a4a276c18eae939a61a2f210
Alan J Santos <alanjosedossantos011@gmail.com>
2022-03-29 19:27:00 -0300

    endpoint sales-api testado

--- sales-api/app.js
+++ sales-api/app.js
@@ -2,7 +2,15 @@ import express from "express";
 
 const app = express();
 const env = process.env;
-const PORT  = env.PORT || 8083;
+const PORT  = env.PORT || 8082;
+
+app.get('/api/status', (req, res) => {
+    return res.status(200).json({
+        service: 'Sales-API',
+        status: 'up',
+        httpStatus: 200,
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server started sucessfully at port ${PORT}`);
