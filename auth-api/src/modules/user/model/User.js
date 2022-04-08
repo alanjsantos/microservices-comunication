d6f13e911fb24813bce176ef60d89c453f305fae
Alan J Santos <alanjosedossantos011@gmail.com>
2022-04-08 09:25:09 -0300

    ajuste app.js

--- auth-api/src/modules/user/model/User.js
+++ auth-api/src/modules/user/model/User.js
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
 import Sequelize from 'sequelize';
 import sequelize from './../../../config/db/dbConfig';
-
-const User = sequelize.define(
-    "user",
-    {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-    },
-    {}
-  );
-
-  export default User;
-=======
-import Sequelize from "sequelize";
-import sequelize from "../../../config/db/dbConfig";
 
 const User = sequelize.define(
     'user', 
@@ -56,4 +26,3 @@ const User = sequelize.define(
 )
 
 export default User
->>>>>>> 6f3a565cf2fcab7739468ebdf1d19d01b6a08276
