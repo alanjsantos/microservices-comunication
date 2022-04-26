f2a5dd960abae5a55b9e85ffd910ce732b114d03
Alan J Santos <alan.jose@b2wdigital.com>
2022-04-26 00:22:47 -0300

    ajuste ProducService

--- product-api/src/main/java/com/alanjsantos/productapi/service/ProductService.java
+++ product-api/src/main/java/com/alanjsantos/productapi/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.alanjsantos.productapi.service;
 
+import com.alanjsantos.productapi.model.Category;
 import com.alanjsantos.productapi.model.Product;
+import com.alanjsantos.productapi.model.Supplier;
 import com.alanjsantos.productapi.repository.ProductRepository;
 import com.alanjsantos.productapi.service.exception.DataIntegrityViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +21,14 @@ public class ProductService {
     private CategoryService categoryService;
 
     public Product save (Product product) {
-        supplierService.getById(product.getSupplier().getId());
-        categoryService.getById(product.getCategory().getId());
+        var sup = supplierService.getById(product.getSupplier().getId());
+        var cat = categoryService.getById(product.getCategory().getId());
 
         if (product.getQuantityAvailable() <= 0) {
             throw new DataIntegrityViolationException("The quantity should not be less or equal to zero");
         }
+        product.setCategory(cat);
+        product.setSupplier(sup);
         return productRepository.save(product);
     }
 
