d2bb94c29675a8769689643507e18159c3ff4330
Alan J Santos <alan.jose@b2wdigital.com>
2022-05-06 15:40:45 -0300

    configurando o RabbitMQ no projeto

---
+++ product-api/src/main/java/com/alanjsantos/productapi/config/RabbitConfig.java
@@ -0,0 +1,63 @@
+package com.alanjsantos.productapi.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Value("${app-config.rabbit.exchange.product}")
+    private String productTopicExchange;
+
+    @Value("${app-config.rabbit.routingKey.product-stock}")
+    private String productStockKey;
+
+    @Value("${app-config.rabbit.routingKey.sales-confirmation}")
+    private String salesConfirmationKey;
+
+    @Value("${app-config.rabbit.queue.product-stock}")
+    private String productSotckMq;
+
+    @Value("${app-config.rabbit.queue.sales-confirmation}")
+    private String salesConfirmationMq;
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange(productTopicExchange);
+    }
+
+    @Bean
+    public Queue productSotckMq() {
+        return new Queue(productSotckMq, true);
+    }
+
+    @Bean
+    public Queue salesConfirmationMq() {
+        return new Queue(salesConfirmationMq, true);
+    }
+
+    public Binding productSotckMqBinding(TopicExchange topicExchange) {
+        return BindingBuilder
+                .bind(productSotckMq())
+                .to(topicExchange)
+                .with(productStockKey);
+    }
+    public Binding salesConfirmationMqBinding(TopicExchange topicExchange) {
+        return BindingBuilder
+                .bind(salesConfirmationMq())
+                .to(topicExchange)
+                .with(salesConfirmationKey);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
