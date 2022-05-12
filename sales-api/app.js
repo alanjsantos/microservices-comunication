dbcdd2fd26fe5277cd6f80c17593de1d36c9d3e6
Alan J Santos <alan.jose@b2wdigital.com>
2022-05-12 16:55:33 -0300

    configurando mongoDB

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,17 +1,20 @@
 import express from "express";
+import { connect } from "./src/config/db/mongoDbConfig";
 
 const app = express();
 const env = process.env;
-const PORT  = env.PORT || 8082;
+const PORT = env.PORT || 8082;
 
-app.get('/api/status', (req, res) => {
-    return res.status(200).json({
-        service: 'Sales-API',
-        status: 'up',
-        httpStatus: 200,
-    })
-})
+connect();
+
+app.get("/api/status", (req, res) => {
+  return res.status(200).json({
+    service: "Sales-API",
+    status: "up",
+    httpStatus: 200,
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started sucessfully at port ${PORT}`);
-})
+  console.log(`Server started sucessfully at port ${PORT}`);
+});
