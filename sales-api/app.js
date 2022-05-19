9c917ae6386409b652092ad74a322aa43ad18d23
Alan J Santos <alan.jose@b2wdigital.com>
2022-05-19 17:46:58 -0300

    configurando o RabbitMQ

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,14 +1,16 @@
 import express from "express";
-import { connect } from "./src/config/db/mongoDbConfig";
 import { createInitialData } from "./src/config/db/intialData";
+import { connectRabbitMq } from "./src/config/rabbitmq/rabbitConfig";
+import { connectMongoDb } from "./src/config/db/mongoDbConfig";
 import checkToken from "./src/config/auth/checkToken";
-
 const app = express();
 const env = process.env;
 const PORT = env.PORT || 8082;
 
-connect();
+connectMongoDb();
 createInitialData();
+connectRabbitMq();
+
 app.use(checkToken);
 
 app.get("/api/status", async (req, res) => {
---
+++ sales-api/src/config/rabbitmq/rabbitConfig.js
@@ -0,0 +1,46 @@
+import amqp from "amqplib/callback_api";
+import { RABBIT_MQ_URL } from "../constants/secrets";
+
+import {
+  PRODUCT_TOPIC,
+  PRODUCT_STOCK_UPDATE_QUEUE,
+  PRODUCT_STOCK_UPDATE_ROUTING_KEY,
+  SALES_CONFIRMATION_QUEUE,
+  SALES_CONFIRMATION_ROUTING_KEY,
+} from "./queue";
+
+const HALF_SECOND = 500;
+
+export async function connectRabbitMq() {
+  amqp.connect(RABBIT_MQ_URL, (error, connection) => {
+    if (error) {
+      throw error;
+    }
+    createQueue(
+      connection,
+      PRODUCT_STOCK_UPDATE_QUEUE,
+      PRODUCT_STOCK_UPDATE_ROUTING_KEY,
+      PRODUCT_TOPIC
+    );
+    createQueue(
+      connection,
+      SALES_CONFIRMATION_QUEUE,
+      SALES_CONFIRMATION_ROUTING_KEY,
+      PRODUCT_TOPIC
+    );
+    setTimeout(function name(params) {
+      connection.close();
+    }, HALF_SECOND);
+  });
+}
+
+function createQueue(connection, queue, routingKey, topic) {
+  connection.createChannel((error, channel) => {
+    if (error) {
+      throw error;
+    }
+    channel.assertExchange(topic, "topic", { durable: true });
+    channel.assertQueue(queue, { durable: true });
+    channel.bindQueue(queue, topic, routingKey);
+  });
+}
